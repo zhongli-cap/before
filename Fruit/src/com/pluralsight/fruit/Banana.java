6dea596fb72ad7d8654307fe1c94038840a4edd0
zhongli-cap <zli@cap.org>
2020-06-17 15:41:59 -0500

    banana fruit

--- Fruit/src/com/pluralsight/fruit/Banana.java
+++ Fruit/src/com/pluralsight/fruit/Banana.java
@@ -2,7 +2,7 @@ package com.pluralsight.fruit;
 
 public class Banana implements IFruit {
 
-    private static final String NAME = "Banana";
+    private static final String NAME = "Banana Fruit";
     private static final int ROLL_MULTIPLIER = 0;
 
     public String getName() {
